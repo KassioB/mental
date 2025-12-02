ca1a765dd9e1b57087c49197df1ed5a99a798d86
Kássio <kbc1@aluno.ifal.edu.br>
2025-12-02 19:32:48 -0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // Substitua 'mental' pelo nome do seu repositório
+  basePath: process.env.NODE_ENV === 'production' ? '/mental' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/mental/' : '',
+
   typescript: {
     ignoreBuildErrors: true,
   },
